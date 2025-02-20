dcad1042628da94b49b532fa5e64307b101f7170
Andriy Sotnyk <lodochnikkot@gmail.com>
2025-02-20 11:14:38 +0200

    most of ui done

--- app.js
+++ app.js
@@ -2,11 +2,24 @@ const THEME = 'theme';
 const LIGHT_THEME = 'theme-white';
 const DARK_TEHME = 'theme-dark';
 
-const container = document.querySelector('main');
+const NOTIFY_HOUR = 'hour';
+const NOTIFY_MINUTE = 'minute';
+
+const container = document.querySelector('body');
 const showMore = document.querySelector('.show-more');
 const view = document.querySelector('.view');
 const viewFrame = view.querySelector('iframe');
 const photoContainer = document.querySelector('.photo-container');
+const mapContainer = document.querySelector('.map-container');
+const notificationContainer = document.querySelector('.notification-popup');
+
+const closeNotification = document.querySelector('.close-notification-popup');
+const showNotification = document.querySelector('.notification-trigger');
+
+const hourSelector = document.querySelector("#hour")
+const minuteSelector = document.querySelector("#minute")
+
+const closeNotificationNoSave = document.querySelector(".close-notification-popup-no-save")
 
 function setTheme(theme) {
     localStorage.setItem(THEME, theme);
@@ -15,15 +28,38 @@ function setTheme(theme) {
     container.classList.add(theme)
 } 
 
+function openView(e) {
+    const target = e.target
+
+    if (target.dataset.viewLink) {
+        viewFrame.src = target.dataset.viewLink
+        view.classList.add('active')
+    }
+}
+
+function saveNotificationTime(hour, minute) {
+    localStorage.setItem(NOTIFY_HOUR, hour);
+    localStorage.setItem(NOTIFY_MINUTE, minute);
+
+    notificationContainer.classList.remove('active')
+}
+
 if (!localStorage.getItem(THEME)) {
     setTheme(LIGHT_THEME)
 } else {
     setTheme(localStorage.getItem(THEME))
 }
 
+if (localStorage.getItem(NOTIFY_HOUR)) {
+    hourSelector.value = localStorage.getItem(NOTIFY_HOUR)
+}
+
+if (localStorage.getItem(NOTIFY_MINUTE)) {
+    minuteSelector.value = localStorage.getItem(NOTIFY_MINUTE)
+}
+
 const toggle = document.querySelector(".toggle");
 const closeView = document.querySelector(".view .close")
-
 
 toggle.addEventListener("click", () => {
     if (localStorage.getItem(THEME) == LIGHT_THEME) {
@@ -43,10 +79,21 @@ closeView.addEventListener("click", () => {
 });
 
 photoContainer.addEventListener("click", e => {
-    const target = e.target
+    openView(e)
+});
 
-    if (target.dataset.viewLink) {
-        viewFrame.src = target.dataset.viewLink
-        view.classList.add('active')
-    }
+mapContainer.addEventListener("click", e => {
+    openView(e)
+})
+
+closeNotification.addEventListener("click", () => {
+    saveNotificationTime(hourSelector.value, minuteSelector.value)
+})
+
+showNotification.addEventListener("click", () => {
+    notificationContainer.classList.add('active');
+})
+
+closeNotificationNoSave.addEventListener("click", () => {
+    notificationContainer.classList.remove('active');
 })
